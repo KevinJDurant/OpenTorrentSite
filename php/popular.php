efdc4e09fbef56475c1a1894ced0f8337cfe001d
Azuka Chan <42724187+AzukaChan@users.noreply.github.com>
2019-05-02 03:57:07 +1000

    Update popular.php

--- php/popular.php
+++ php/popular.php
@@ -11,13 +11,13 @@
 	 */
 
 	/**
-	 * Gets the 10 most seeded torrents of a category of the last 7 days.
+	 * Gets the 10 most seeded torrents of a category of the last 365 days.
 	 *
 	 * @return array.
 	 */
 	 function get_popular_per_cat($cat,$db) {
 		 $catID = $cat;
-		 $torrents = $db -> select("SELECT DATE_FORMAT(t.uploaddate, '%m/%d/%Y'), t.userid,t.categoryid,t.name,t.uploaddate,t.size,t.seeders,t.leechers,t.hash,t.magnet,u.username,u.uploaderstatus,c.id,c.categoryname FROM `torrents` t INNER JOIN `users` u ON t.userid=u.user_id INNER JOIN `categories` c ON t.categoryid=c.id WHERE (t.uploaddate BETWEEN NOW() - INTERVAL 7 DAY AND NOW()) AND (t.categoryid=$catID) ORDER by t.seeders desc LIMIT 10");
+		 $torrents = $db -> select("SELECT DATE_FORMAT(t.uploaddate, '%m/%d/%Y'), t.userid,t.categoryid,t.name,t.uploaddate,t.size,t.seeders,t.leechers,t.hash,t.magnet,u.username,u.uploaderstatus,c.id,c.categoryname FROM `torrents` t INNER JOIN `users` u ON t.userid=u.user_id INNER JOIN `categories` c ON t.categoryid=c.id WHERE (t.uploaddate BETWEEN NOW() - INTERVAL 365 DAY AND NOW()) AND (t.categoryid=$catID) ORDER by t.seeders desc LIMIT 10");
 		 return $torrents;
 	 }
 
