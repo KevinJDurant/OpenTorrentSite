0c1191f43787c2f0da21c047a712c79a186edd31
Azuka Chan <42724187+AzukaChan@users.noreply.github.com>
2019-05-09 18:35:20 +1000

    Trusted/VIP user icon and other fixes
    Finally getting the hang of GitHub..
    Sorry for requesting and cancelling pulls so often.
    
    1) Added trusted/vip icons for users with `uploaderstatus` 2 (trusted) and 3 (vip) next to uploader name on torrent.php
    2) Fixed a bunch of spelling mistakes
    3) Re-added itorrent download links (didnt realize I messed this up when you pulled last time)

--- en/view/my-torrents.php
+++ en/view/my-torrents.php
@@ -188,6 +188,7 @@
                             <td data-label="Leech">'.$row["leechers"].'</td>
                             <td data-label="Download">
                                 <a href="'.$row["magnet"].'"><span class="glyphicon glyphicon-magnet link"></span></a>
+								<a href="http://itorrents.org/torrent/'.$row["hash"].'.torrent"><span class="glyphicon glyphicon-download-alt link"></span></a>
                                 <a href="delete_torrent_file.php?delete_id='.$row["torrents_id"].'"  class="delete" data-confirm="Are you sure you want to permanently remove this torrent from the database?"><span class="glyphicon glyphicon-trash link"></span></a>
                             </td>
                         </tr>';
