2fb15fd2c6c34c5db2749779f2fb69560d4399f6
Azuka Chan <42724187+AzukaChan@users.noreply.github.com>
2020-01-06 17:27:34 +1000

    Update config.ini.php
    Fixed formatting

--- config/config.ini.php
+++ config/config.ini.php
@@ -5,14 +5,11 @@
 username = root
 password = mysql
 dbname = opentorrentsite
-
 [website]
 footer = "Copyright &copy; Your Website 2019"
 name = "OpenTorrentSite"
-
 [meta]
 meta_description = "OpenTorrentSite: an easy to setup torrent website!"
-
 [Google Analytics ID]
 google_id = ""
 ;*/
