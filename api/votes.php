81afe4a1dd788735f91cc43d74f70acfa02aca18
Azuka Chan <42724187+AzukaChan@users.noreply.github.com>
2020-07-26 05:39:54 +1000

    Update votes.php
    Fixed the "users can have positive and negative vote" bug

--- api/votes.php
+++ api/votes.php
@@ -15,18 +15,18 @@
 	$userid=$_POST['userid'];
 
     $queryinsert = "INSERT INTO `votes` (`id`, `torrentid`, `userid`, `hasvoted`) VALUES (null, '$torrentid', '$userid', '$do')";
-    $querycheck = "select count(id) as c from `votes` where `torrentid`='$torrentid' and `userid`='$userid' and `hasvoted`='$do'";
-    $querydelete = "delete from `votes` where `torrentid`='$torrentid' and `userid`='$userid' and `hasvoted`='$do'";
+    $querycheck = "select count(id) as c from `votes` where `torrentid`='$torrentid' and `userid`='$userid'";
+    $updatequery = "update `votes` set `hasvoted`='$do' where `torrentid`='$torrentid' and `userid`='$userid'";
 	
 	$count=($db->select($querycheck)[0]['c']);
 	if($count!=null && $count>0){
-		$db->query($querydelete);
+		$db->query($updatequery);
 	}else{
 		$db->query($queryinsert);
 	}
 		$votesQ = $db->select("SELECT SUM(hasvoted) AS `Total_Votes` FROM `votes` WHERE `torrentid`=".$torrentid."");
 		if ($votesQ[0]["Total_Votes"] === NULL) {
-        $votes = 0;
+			$votes = 0;
 		}else{
 			$votes=$votesQ[0]["Total_Votes"];
 		}
