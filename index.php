88e85023290b7a2031eb9507a55938258bd2660a
Azuka Chan <42724187+AzukaChan@users.noreply.github.com>
2020-12-29 02:59:57 +1000

    Update index.php
    Invite link fix

--- index.php
+++ index.php
@@ -97,6 +97,9 @@
                                         <a href="en/view/preferences.php"><span class="glyphicon glyphicon-cog"></span> Preferences</a>
                                     </li>
                                     <li>
+                                        <a href="en/view/invitations.php"><span class="glyphicon glyphicon-pencil"></span> Invites</a>
+                                    </li>
+                                    <li>
                                         <a href="/en/account/logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a>
                                     </li>
                                 </ul>
