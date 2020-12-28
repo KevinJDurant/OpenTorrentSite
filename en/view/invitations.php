708d2312312e2acf77ea32302d4e5f4c5266b40b
Azuka Chan <42724187+AzukaChan@users.noreply.github.com>
2020-12-29 02:55:15 +1000

    Update invitations.php
    link fix

--- en/view/invitations.php
+++ en/view/invitations.php
@@ -139,10 +139,10 @@
                                         <a href="my-torrents.php"><span class="glyphicon glyphicon-book"></span> Torrents</a>
                                     </li>
                                     <li>
-                                        <a href="#"><span class="glyphicon glyphicon-cog"></span> Preferences</a>
+                                        <a href="preferences.php"><span class="glyphicon glyphicon-cog"></span> Preferences</a>
                                     </li>
                                     <li>
-                                        <a href="invitations.php"><span class="glyphicon glyphicon-cog"></span> Invites</a>
+                                        <a href="#"><span class="glyphicon glyphicon-pencil"></span> Invites</a>
                                     </li>
                                     <li>
                                         <a href="../account/logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a>
